446598213d9f11a75628d1429f82de1c8d481d3d
Alejandro Perdomo <aperdomob@psl.com.co>
2020-03-25 19:36:54 -0500

    feat(repository): add endpoints

--- src/github/controller/repository/repository.controller.ts
+++ src/github/controller/repository/repository.controller.ts
@@ -1,12 +1,71 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Query, Param } from '@nestjs/common';
 import { RepositoryService } from 'src/github/service/repository/repository.service';
 
 @Controller('repositories')
 export class RepositoryController {
-  constructor(private readonly repositoryService: RepositoryService) {}
+  constructor(private readonly repositoryService: RepositoryService) { }
 
   @Get()
   public getAllRepositories() {
-    return this.repositoryService.scan();
+    return [{
+      name: 'test',
+      diagnostic: {
+        total: 19,
+        ok: 14,
+        warning: 0,
+        fail: 5
+      }
+    }, {
+      name: 'my-repo',
+      diagnostic: {
+        total: 13,
+        ok: 4,
+        warning: 4,
+        fail: 5
+      }
+    }]
   }
+
+  @Post('/scan')
+  public scanRepositorioes() {
+    // scan all repositoriies from organization
+  }
+
+  @Get((':repositoryName'))
+  public getRepositoryDetail(@Param() params) {
+    return {
+      name: params.repositoryName,
+      diagnostic: {
+        rules: [{
+          "name": "security-alerts",
+          "result": "OK"
+        },
+        {
+          "name": "allow-commits",
+          "result": "OK"
+        },
+        {
+          "name": "disabled-squash",
+          "result": "FAILED",
+          "error": {
+            "message": "the squash buttons is enable: expected true to be false",
+            "actual": true,
+            "expected": false
+          }
+        },
+        {
+          "name": "disabled-rebase",
+          "result": "FAILED",
+          "error": {
+            "message": "the rebase buttons is enable: expected true to be false",
+            "actual": true,
+            "expected": false
+          }
+        }],
+        total: 19,
+        ok: 14,
+        fail: 5
+      }
+    }
+  };
 }
